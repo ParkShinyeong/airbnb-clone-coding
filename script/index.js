1151b89f65c33f578525459f1bdc36d059b77876
ParkShinyeong <syngh503@gmail.com>
2023-04-12 23:14:06 +0900

    Feat/click heart icon
    하트 아이콘 클릭 시 활성화하는 로직 추가

--- script/index.js
+++ script/index.js
@@ -3,5 +3,17 @@ document.querySelectorAll(".category-button").forEach(button => {
         document.querySelector("#active-category-button").removeAttribute("id")
         button.setAttribute('id', 'active-category-button')
     })
-    
 });
+
+
+// 하트 아이콘 클릭 시 하트 색 변경
+document.querySelectorAll(".place-image-wrapper").forEach(heartButton => {
+    heartButton.addEventListener("click", event => {
+        if (heartButton.classList.contains("active-heart-button")) {
+            heartButton.classList.remove("active-heart-button")            
+        }
+        else {
+            heartButton.classList.add("active-heart-button")
+        }
+    })
+})
